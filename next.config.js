92d0fa673a0cfcdf08782f7578dbf7afcdca19ab
ingedevogelaere-hub <devogelaere.inge@gmail.com>
2026-05-20 09:50:54 +0200

    Refactor next.config.js to export configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,2 @@
-import '../styles/globals.css';
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const nextConfig = { reactStrictMode: true }
+module.exports = nextConfig
